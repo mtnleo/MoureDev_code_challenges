505e89116419f2ba5d0810b91ae2a5b52ff3c157
mtnleo <mtnleonardi@gmail.com>
2022-12-21 21:17:01 -0300

    Counting words challenge completed

---
+++ Counting Words/main.py
@@ -0,0 +1,19 @@
+## Create a function that counts how many times a word is repeated
+## in the given text
+## You can't use language's own funcs that automatically solves it
+
+def count_words_in_text(text):
+    words_count = {}
+    words = text.lower().split(" ")
+
+    for word in words:
+        if word in words_count:
+            words_count[word] += 1
+        else:
+            words_count.update({word: 1})
+
+    return words_count
+
+if __name__ == "__main__":
+    words_count = count_words_in_text(input("Input text: "))
+    print(words_count)
